2cc8b756182744905b60a44d7f7a1988dec4fd90
Khánh Nguyễn <duykhanhduong088@gmail.com>
2025-10-20 22:33:14 +0700

    day01

--- lesson01-spring-boot/lesson01-spring-boot/src/test/java/com/devmaster/lesson01_spring_boot/pkg_default_method/Shape.java
+++ lesson01-spring-boot/lesson01-spring-boot/src/test/java/com/devmaster/lesson01_spring_boot/pkg_default_method/Shape.java
@@ -1,13 +1,31 @@
 package com.devmaster.lesson01_spring_boot.pkg_default_method;
 
-// Interface Shape với phương thức trừu tượng và phương thức default
 public interface Shape {
-
-    // Phương thức trừu tượng: mỗi hình sẽ có cách vẽ riêng
     void draw();
 
-    // Phương thức default: có sẵn nội dung, lớp con có thể dùng hoặc ghi đè
+    // Thêm hỗ trợ màu bằng mã ANSI
     default void setColor(String color) {
-        System.out.println("Vẽ hình với màu " + color);
+        String colorCode;
+
+        switch (color.toLowerCase()) {
+            case "đỏ":
+                colorCode = "\u001B[31m"; // Red
+                break;
+            case "xanh":
+                colorCode = "\u001B[34m"; // Blue
+                break;
+            case "vàng":
+                colorCode = "\u001B[33m"; // Yellow
+                break;
+            case "lục":
+            case "xanh lá":
+                colorCode = "\u001B[32m"; // Green
+                break;
+            default:
+                colorCode = "\u001B[0m"; // Reset
+        }
+
+        // In ra chữ màu + reset về bình thường
+        System.out.println(colorCode + "Vẽ hình với màu: " + color + "\u001B[0m");
     }
 }
