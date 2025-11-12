b47173362eb16cfb8c3a2aab7cdcebdcaad1df2f
Khánh Nguyễn <duykhanhduong088@gmail.com>
2025-11-12 11:24:52 +0700

    day04

---
+++ k23cnt2_ndkday04/k23cnt2_ndkday04/src/main/java/com/example/k23cnt2_ndkday04/K23cnt2Ndkday04Application.java
@@ -0,0 +1,13 @@
+package com.example.k23cnt2_ndkday04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K23cnt2Ndkday04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K23cnt2Ndkday04Application.class, args);
+	}
+
+}
---
+++ k23cnt2_ndkday04/k23cnt2_ndkday04/src/test/java/com/example/k23cnt2_ndkday04/K23cnt2Ndkday04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k23cnt2_ndkday04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K23cnt2Ndkday04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
